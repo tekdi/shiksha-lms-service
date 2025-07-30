f028b9116bf1951ce2b99cb4b098cfc5e3b05f82
Vinayak Patil <vinayak.patil@tekditechnologies.com>
2025-07-30 12:09:09 +0530

    Task #244524: Ordering Service Implementation

--- app/src/common/common.module.ts
+++ app/src/common/common.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigurationModule } from '../configuration/configuration.module';  
 import { TenantModule } from './tenant/tenant.module';
+import { OrderingService } from './services/ordering.service';
+import { Module as ModuleEntity } from '../modules/entities/module.entity';
+import { Lesson } from '../lessons/entities/lesson.entity';
 
 @Module({
   imports: [
     ConfigModule,
     ConfigurationModule,
     TenantModule,
+    TypeOrmModule.forFeature([ModuleEntity, Lesson]),
   ],
-  exports: [],
+  providers: [OrderingService],
+  exports: [OrderingService],
 })
 export class CommonModule {}
